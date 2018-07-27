57465509c5beae8fc87693dcbb23f19f2d56276f
bombadillo <mumfordnsons@gmail.com>
2018-07-27 22:35:14 +0100

    Add foo route. Set base path to bikestats

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Foo from '@/components/Foo'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: '/bikestats/',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/foo',
+      name: 'Foo',
+      component: Foo
     }
   ]
 })
