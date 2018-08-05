7e99d97f424a74c23a6ba93216b28734b72d9321
bombadillo <mumfordnsons@gmail.com>
2018-08-05 21:04:46 +0100

    Remove miles suffix from bikes

--- src/components/bikes/Bikes.vue
+++ src/components/bikes/Bikes.vue
@@ -17,7 +17,7 @@
         <div class="px-6 pt-4 pb-6 text-center">
           <strong>{{bike.name}}</strong>
           <div class="text-grey-darkest mt-2">
-            {{convertToMiles(bike.distance)}} miles
+            {{convertToMiles(bike.distance)}}
           </div>
 
           <div v-if="bike.primary" class="primary bg-indigo-darker text-white text-xs tracking-wide px-2 py-1">primary</div>
