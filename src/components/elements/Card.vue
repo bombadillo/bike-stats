2e32c18e56c0af7e02f72411660fb1b0f15ff108
bombadillo <mumfordnsons@gmail.com>
2018-08-07 19:57:18 +0100

    Move white background to card element

--- src/components/elements/Card.vue
+++ src/components/elements/Card.vue
@@ -1,8 +1,8 @@
 
 
 <template>
-  <div class="flex mb-4 justify-center bg-white">
-    <div class="lg:w-1/2 py-6 px-4 rounded overflow-hidden shadow-lg mx-6 my-2 lg:mt-12 w-full relative">
+  <div class="flex mb-4 justify-center">
+    <div class="lg:w-1/2 py-6 px-4 rounded overflow-hidden shadow-lg mx-6 my-2 lg:mt-12 w-full relative bg-white">
       <slot></slot>
     </div>
   </div>
