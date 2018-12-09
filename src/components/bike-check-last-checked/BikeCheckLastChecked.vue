3d50839a5fc7464f3e32d0596b9f056542a2264b
bombadillo <mumfordnsons@gmail.com>
2018-12-09 22:21:02 +0000

    Programatically set progress bar colour

--- src/components/bike-check-last-checked/BikeCheckLastChecked.vue
+++ src/components/bike-check-last-checked/BikeCheckLastChecked.vue
@@ -6,15 +6,25 @@
     >
       <div v-if="lastCheckedPercentage > 0 && lastCheckedPercentage < 100">
         <div
-          :class="`h-2 mt-4 bg-green-light`"
+          :class="`h-2 mt-4 bg-${progressColour}-light`"
           :style="{ width: lastCheckedPercentage + '%' }"
         ></div>
       </div>
-      <div v-if="lastCheckedPercentage == 0">
-        <div class="mt-4 text-green-light"> <i class="far fa-check-circle"></i> 0 miles since last check</div>
-      </div>
-      <div v-if="lastCheckedPercentage > 0 && lastCheckedPercentage < 70">
+
+      <div v-if="lastCheckedPercentage >= 0 && lastCheckedPercentage < 70">
         <div class="mt-4 text-green-light"> <i class="far fa-check-circle"></i> {{milesSinceLastCheck}} miles since last check</div>
+      </div>
+
+      <div v-if="lastCheckedPercentage >= 70 && lastCheckedPercentage < 100">
+        <div class="mt-4 text-orange-base"> <i class="far fa-check-circle"></i> {{milesSinceLastCheck}} miles since last check</div>
+      </div>
+
+      <div
+        v-if="lastCheckedPercentage >= 100"
+        class="w-full"
+      >
+        <div class="h-2 mt-4 bg-red-light w-ful"></div>
+        <div class="mt-4 text-red-light"> <i class="fas fa-exclamation"></i> Hasn't been checked in {{milesSinceLastCheckReadable}}!</div>
       </div>
     </div>
     <div
@@ -41,23 +51,41 @@ export default {
   data: function() {
     return {
       lastCheckedPercentage: undefined,
-      milesSinceLastCheck: undefined
+      milesSinceLastCheck: undefined,
+      milesSinceLastCheckReadable: undefined,
+      progressColour: undefined
     }
   },
   methods: {
     processLastCheck: function(check, currentDistance) {
       this.getMilesSinceCheck(check, currentDistance)
       this.getPercentageSinceLastCheck(check, currentDistance)
+      this.calculateProgressColour()
     },
     getMilesSinceCheck: function(check, currentDistance) {
       this.milesSinceLastCheck = convertor.metreToMileValue(
         currentDistance - check.distance
       )
+      this.milesSinceLastCheckReadable = convertor.metreToMile(
+        currentDistance - check.distance
+      )
     },
     getPercentageSinceLastCheck: function(check, currentDistance) {
-      let percentage = (this.milesSinceLastCheck / check.distanceInterval) * 100
+      let percentage = Math.ceil(
+        (this.milesSinceLastCheck / check.distanceInterval) * 100
+      )
 
       this.lastCheckedPercentage = percentage
+    },
+    calculateProgressColour: function() {
+      if (this.lastCheckedPercentage >= 0 && this.lastCheckedPercentage < 70) {
+        this.progressColour = 'green'
+      } else if (
+        this.lastCheckedPercentage >= 70 &&
+        this.lastCheckedPercentage < 100
+      ) {
+        this.progressColour = 'orange'
+      }
     },
     formatDate: date => dateFormatter.format(date)
   },
