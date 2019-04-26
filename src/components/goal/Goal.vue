012e8e22b4bad564753b21742cf16c0ff6d1facf
bombadillo <mumfordnsons@gmail.com>
2019-04-26 23:48:01 +0100

    Add pace calculation to goal

--- src/components/goal/Goal.vue
+++ src/components/goal/Goal.vue
@@ -3,11 +3,15 @@
     <h2 class="no-margin border-solid border-b border-solid border-grey-lighter pb-3 text-center">
       <i class="fas fa-bullseye"></i> Goal
     </h2>
-    <h6 class="text-center ">{{currentDistanceInMiles}}/{{goal}} miles</h6>
+
     <div
       :class="`h-2 mt-4 bg-${progressColour}-light`"
-      :style="{ width: lastCheckedPercentage + '%' }"
+      :style="{ width: goalCompletedPercentage + '%' }"
     ></div>
+
+    <h6 class="text-center ">{{currentDistanceInMiles}}/{{goal}} miles</h6>
+    <h6 class="text-center ">{{pace}} miles/pw {{ pace > 0 ? 'ahead of pace' : 'behind pace' }}</h6>
+
   </Card>
 </template>
 
@@ -20,8 +24,9 @@ export default {
   data() {
     return {
       progressColour: 'green',
-      lastCheckedPercentage: 80,
-      currentDistanceInMiles: undefined
+      currentDistanceInMiles: undefined,
+      goalCompletedPercentage: undefined,
+      pace: undefined
     }
   },
   methods: {
@@ -30,12 +35,29 @@ export default {
         (this.currentDistanceInMiles / this.goal) * 100
       )
 
-      this.lastCheckedPercentage = percentage
+      this.goalCompletedPercentage = percentage
+    },
+    calculatePace: function() {
+      var now = new Date()
+      var start = new Date(now.getFullYear(), 0, 0)
+      var diff = now - start
+      var oneDay = 1000 * 60 * 60 * 24
+      var day = Math.floor(diff / oneDay)
+      var weeksPassed = day / 7
+      var weeksLeft = 52 - weeksPassed
+      var averageWeeklyDistance = Math.floor(
+        this.currentDistanceInMiles / weeksPassed
+      )
+
+      var goalDifference = this.goal - this.currentDistanceInMiles
+      var paceRequired = Math.floor(goalDifference / weeksLeft)
+      this.pace = averageWeeklyDistance - paceRequired
     }
   },
   created: function() {
     this.currentDistanceInMiles = convertor.metreToMileValue(this.current)
     this.getPercentageAcheived()
+    this.calculatePace()
   }
 }
 </script>
