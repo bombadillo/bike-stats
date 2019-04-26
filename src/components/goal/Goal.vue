6fc6daa971b2173a6278723cfce57145482bc0d0
bombadillo <mumfordnsons@gmail.com>
2019-04-27 00:05:06 +0100

    Improve goal visual

--- src/components/goal/Goal.vue
+++ src/components/goal/Goal.vue
@@ -1,16 +1,24 @@
 <template>
   <Card>
-    <h2 class="no-margin border-solid border-b border-solid border-grey-lighter pb-3 text-center">
-      <i class="fas fa-bullseye"></i> Goal
-    </h2>
+    <div class="border-solid border-b border-solid border-grey-lighter pb-3">
+      <h2 class="no-margin text-center">
+        <i class="fas fa-bullseye"></i> Goal
+      </h2>
+
+      <h4 class="text-center mt-2 text-grey-dark">{{currentDistanceInMiles}}/{{goal}} miles</h4>
+    </div>
 
     <div
       :class="`h-2 mt-4 bg-${progressColour}-light`"
       :style="{ width: goalCompletedPercentage + '%' }"
     ></div>
 
-    <h6 class="text-center ">{{currentDistanceInMiles}}/{{goal}} miles</h6>
-    <h6 class="text-center ">{{pace}} miles/pw {{ pace > 0 ? 'ahead of pace' : 'behind pace' }}</h6>
+    <h6 class="text-center mt-4"><span
+        class="rounded-full px-2 py-1 text-xs font-bold text-white"
+        v-bind:class="{'bg-green': pace > 0, 'bg-red': pace < 0 }"
+      >
+        {{Math.abs(pace)}} miles {{ pace > 0 ? 'ahead of pace' : 'behind pace' }}</span>
+    </h6>
 
   </Card>
 </template>
