db0cdb2a456e3584a896a8eb4491db7457cff858
bombadillo <mumfordnsons@gmail.com>
2019-05-13 19:40:25 +0100

    Add activities to mobile nav

--- src/components/header/HeaderMobileMenu.vue
+++ src/components/header/HeaderMobileMenu.vue
@@ -4,23 +4,23 @@
       <router-link
         to="/"
         class="link block text-white hover:text-white no-underline mt-1"
-        >Home</router-link
-      >
+      >Home</router-link>
+      <router-link
+        to="Activities"
+        class="link block text-white hover:text-white no-underline mt-1"
+      >Activities</router-link>
       <router-link
         to="/profile"
         class="link block text-white hover:text-white no-underline mt-1"
-        >My Profile</router-link
-      >
+      >My Profile</router-link>
       <router-link
         to="/bikes"
         class="link block text-white hover:text-white no-underline mt-1"
-        >My Bikes</router-link
-      >
+      >My Bikes</router-link>
       <router-link
         to="/logout"
         class="link block text-white hover:text-white no-underline mt-1"
-        >Logout</router-link
-      >
+      >Logout</router-link>
     </div>
   </div>
 </template>
