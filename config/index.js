39d73ac0055d9242712a2c03e2075f01e094e67d
bombadillo <mumfordnsons@gmail.com>
2019-05-13 20:20:58 +0100

    Fix webpack prod build

--- config/index.js
+++ config/index.js
@@ -34,6 +34,30 @@ module.exports = {
     cssSourceMap: true
   },
 
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
